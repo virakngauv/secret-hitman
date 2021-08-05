7da77aaf3c687d87405d713eb4827af4a963a2ce
Virak Ngauv <virakngauv@gmail.com>
2021-08-05 10:50:33 -0400

    Load basic example to investigate restarts

--- test-application-local.js
+++ test-application-local.js
@@ -1,64 +1,64 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// app.listen(80);
-
-// Start socket.io example
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const app = express();
-const http = require('http');
-const https = require('https');
-const { Server } = require("socket.io");
-
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/codenames-hitman.com/privkey.pem');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/codenames-hitman.com/fullchain.pem');
-
-const credentials = {
-  key: privateKey,
-	cert: certificate
-}
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-const io = new Server();
-io.attach(httpServer);
-io.attach(httpsServer);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+app.listen(80);
 
-server.listen(80, () => {
-  console.log('listening on *:80');
-});
+// // Start socket.io example
+// const express = require('express');
+// const fs = require('fs');
+// const path = require('path');
+// const app = express();
+// const http = require('http');
+// const https = require('https');
+// const { Server } = require("socket.io");
 
-server.listen(443, () => {
-  console.log('listening on *:443');
-});
-// End socket.io example
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/codenames-hitman.com/privkey.pem');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/codenames-hitman.com/fullchain.pem');
 
-// Start latency test
-// server-side
-io.on("connection", (socket) => {
-  socket.on("ping", (cb) => {
-    if (typeof cb === "function")
-      cb();
-  });
-});
-// End latency test
+// const credentials = {
+//   key: privateKey,
+// 	cert: certificate
+// }
+
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(credentials, app);
+
+// const io = new Server();
+// io.attach(httpServer);
+// io.attach(httpsServer);
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
+
+// server.listen(80, () => {
+//   console.log('listening on *:80');
+// });
+
+// server.listen(443, () => {
+//   console.log('listening on *:443');
+// });
+// // End socket.io example
+
+// // Start latency test
+// // server-side
+// io.on("connection", (socket) => {
+//   socket.on("ping", (cb) => {
+//     if (typeof cb === "function")
+//       cb();
+//   });
+// });
+// // End latency test
