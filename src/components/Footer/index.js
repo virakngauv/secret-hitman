bf6ec5404941e595d9c832b25577ac609f87828a
Virak Ngauv <virakngauv@gmail.com>
2021-09-01 14:46:12 -0400

    Be able to console log the hint and hintNumber

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,3 @@
-import InputGroup from "react-bootstrap/InputGroup";
 import FooterClue from "../FooterClue";
 import FooterGuesser from "../FooterGuesser";
 import "./index.css";
@@ -7,9 +6,7 @@ function Footer(props) {
   const isCodemaster = props.isCodemaster;
 
   return (
-    <InputGroup size="sm" className="footer ms-auto me-auto">
-      {isCodemaster ? <FooterClue /> : <FooterGuesser />}
-    </InputGroup>
+    isCodemaster ? <FooterClue /> : <FooterGuesser />
   );
 }
 
--- src/components/FooterClue/index.js
+++ src/components/FooterClue/index.js
@@ -1,34 +1,60 @@
-import { DropdownButton, FormControl, Button, Dropdown } from "react-bootstrap";
+import { DropdownButton, FormControl, Button, Dropdown, Form } from "react-bootstrap";
 import { useState } from "react";
+import InputGroup from "react-bootstrap/InputGroup";
 
-function FooterClue() {
-    const [hintNumber, setHintNumber] = useState("#");
+function FooterClue(props) {
+  const [hint, setHint] = useState("");
+  const [hintNumber, setHintNumber] = useState("#");
 
-    return (
-        <>
-            <FormControl placeholder="Type your hint.." />
-            <DropdownButton
-                variant="outline-secondary"
-                title={hintNumber}
-                id="hint-number-dropdown"
-                onSelect={(eventKey) => {
-                    setHintNumber(eventKey);
-                }}
-            >
-                <Dropdown.Item eventKey="Infinity">Infinity</Dropdown.Item>
-                <Dropdown.Item eventKey="Zero">Zero</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="6">6</Dropdown.Item>
-                <Dropdown.Item eventKey="5">5</Dropdown.Item>
-                <Dropdown.Item eventKey="4">4</Dropdown.Item>
-                <Dropdown.Item eventKey="3">3</Dropdown.Item>
-                <Dropdown.Item eventKey="2">2</Dropdown.Item>
-                <Dropdown.Item eventKey="1">1</Dropdown.Item>
-            </DropdownButton>
-            <Button variant="outline-secondary" id="submit-clue-button">Submit</Button>
-        </>
-    );
+  const handleHintChange = (e) => {
+    setHint(e.target.value);
   }
-  
-  export default FooterClue;
-  
+
+  const handleHintNumberChange = (hintNumber) => {
+    setHintNumber(hintNumber);
+  }
+
+  // This will eventually come from parent props
+  const submitClue = () => console.log(hint, hintNumber);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    submitClue();
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <InputGroup size="sm" className="footer ms-auto me-auto">
+        <FormControl 
+          name="hint" 
+          placeholder="Type your hint.." 
+          onChange={handleHintChange}
+        />
+        <DropdownButton
+          name="hintNumber"
+          variant="outline-secondary"
+          title={hintNumber}
+          id="hint-number-dropdown"
+          onSelect={(eventKey) => {
+            handleHintNumberChange(eventKey);
+          }}
+        >
+          <Dropdown.Item eventKey="Infinity">Infinity</Dropdown.Item>
+          <Dropdown.Item eventKey="Zero">Zero</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="6">6</Dropdown.Item>
+          <Dropdown.Item eventKey="5">5</Dropdown.Item>
+          <Dropdown.Item eventKey="4">4</Dropdown.Item>
+          <Dropdown.Item eventKey="3">3</Dropdown.Item>
+          <Dropdown.Item eventKey="2">2</Dropdown.Item>
+          <Dropdown.Item eventKey="1">1</Dropdown.Item>
+        </DropdownButton>
+        <Button type="submit" variant="outline-secondary" id="submit-clue-button">Submit</Button>
+      </InputGroup>
+    </Form>
+  );
+}
+
+export default FooterClue;
