fc029068b4ab0b3445e4e55ecbc85f235b262edb
Virak Ngauv <virakngauv@gmail.com>
2021-10-21 18:50:09 -0400

    Update gratitude link and footer

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,5 +1,24 @@
 import "./index.css";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, OverlayTrigger, Popover } from "react-bootstrap";
+
+const gratitudePopover = (
+  <Popover id="gratitude-popover">
+    <Popover.Body className="gratitude-popover-text">
+      {`Kim, Michelle, and Pearline for UX/UI feedback. Jenny, Cynthia, and Sovathya for early design questioning. Truc for encouragement. Theo for poking around. And all my playtesters! (I think that includes you?)`}
+    </Popover.Body>
+  </Popover>
+);
+
+const GratitudeLink = ({children}) => (
+  <OverlayTrigger trigger="click" placement="top" overlay={gratitudePopover} rootClose>
+    <button
+      type="button"
+      className="link-button"
+    >
+      {children}
+    </button>
+  </OverlayTrigger>
+);
 
 function AppFooter() {
   return (
@@ -8,9 +27,9 @@ function AppFooter() {
       <Row>
         <Col className="d-flex">
           <div className="app-footer-text ms-auto me-auto text-center">
-            Inspired by <a href="https://spyfall.tannerkrewson.com/">Spyfall</a> <br />
-            with help from these fine <a href="/#">folks</a><br />
-            and managed on <a href="https://github.com/virakngauv/secret-hitman/">GitHub</a>.
+            Inspired by <a href="https://spyfall.tannerkrewson.com/">Spyfall</a>🕵️<br />
+            with help from these fine <GratitudeLink>folks</GratitudeLink>❤️<br />
+            and managed on <a href="https://github.com/virakngauv/secret-hitman/">GitHub</a>💾.
           </div>
         </Col>
       </Row>
