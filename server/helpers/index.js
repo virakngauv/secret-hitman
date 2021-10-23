91b403cf20c74453c8b6d1dcc1d1c5ca4630d673
Virak Ngauv <virakngauv@gmail.com>
2021-10-22 20:16:24 -0400

    Update room code algo

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,17 +1,22 @@
 export function generateRandomRoomCode() {
   // TODO: filter against a bad word filter
   const allowed = "bcdfghkpqrstvz";
-  const vowels = "aeiou";
+  // const vowels = "aeiou";
   const lastCharacter = "23456789y";
 
   let randomRoomCode = "";
-  
+
   randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
-  randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
   randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
-  randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
   randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
-  randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
+  randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
+
+  // randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
+  // randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
+  // randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
+  // randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
+  // randomRoomCode += allowed.charAt(Math.floor(Math.random() * allowed.length));
+  // randomRoomCode += vowels.charAt(Math.floor(Math.random() * vowels.length));
   randomRoomCode += lastCharacter.charAt(Math.floor(Math.random() * lastCharacter.length));
   
   return randomRoomCode;
